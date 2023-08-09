351af5c08409793256d817da97b31c5cca9696ef
britko <britko@naver.com>
2023-08-09 20:27:46 +0900

    [+]메인페이지

---
+++ src/main/java/com/example/cm/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.cm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello.html";
+    }
+
+}
